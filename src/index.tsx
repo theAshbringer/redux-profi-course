2b48d856c05f2baf8d1089732dd063a30d69db9e
theAsbringer <kristya1310@gmail.com>
2022-10-22 16:49:00 +0700

    feat: setup store

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { setupStore } from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = setupStore();
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
